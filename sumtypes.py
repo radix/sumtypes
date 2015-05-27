7871cfc60f5a582ae14c258303f902c94b38f482
Christopher Armstrong <radix@twistedmatrix.com>
2015-05-27 16:06:15 -0500

    Sum types for Python.

---
+++ sumtypes.py
@@ -0,0 +1,89 @@
+"""
+Sum Types (aka Tagged Unions) for Python.
+
+Decorate your classes to make them a sum type::
+
+    @sumtype
+    class MyType(object):
+        MyConstructor = constructor(int)
+        @constructor
+        def AnotherConstructor(x, y):
+            assert type(x) is str
+            assert type(y) is int
+            return (x, y)
+
+Then construct them by calling the constructors::
+
+    v = MyType.MyConstructor(1)
+    v2 = MyType.AnotherConstructor('foo', 2)
+
+You can get the value from the tagged objects::
+
+    assert v.value == 1
+    assert v2.value == ('foo', 2)
+
+The type of the tagged objects is the type you declared::
+
+    assert type(v) is MyType
+
+You can also introspect the constructor used::
+
+    assert v.constructor is MyType.MyConstructor
+
+And the tagged objects have equality semantics, which you can use to do pattern
+matching::
+
+    assert v == MyType.MyConstructor(1)
+    assert v != MyType.MyConstructor(2)
+"""
+
+
+class constructor(object):
+    """
+    A wrapper/decorator to indicate that some callable is a constructor for the
+    sum type that it's attached to.
+    """
+    def __init__(self, callable_):
+        self.callable_ = callable_
+
+    def set_klass(self, klass):
+        self.klass = klass
+
+    def set_name(self, name):
+        self.name = name
+
+    def __call__(self, *args, **kwargs):
+        obj = object.__new__(self.klass)
+        obj.value = self.callable_(*args, **kwargs)
+        obj.constructor = self
+        return obj
+
+
+def sumtype(klass):
+    """
+    A class decorator that treats the class like a sum type.
+
+    Constructors should be wrapped/decorated with :obj:`constructor`.
+    """
+    def __repr__(inst):
+        return "<%s.%s %r>" % (klass.__name__,
+                               inst.constructor.name,
+                               inst.value)
+    klass.__repr__ = __repr__
+
+    def __eq__(inst, other):
+        print "__eq__", inst, other
+        return (inst.constructor is other.constructor
+                and inst.value == other.value)
+    klass.__eq__ = __eq__
+
+    def __ne__(inst, other):
+        print "__ne__", inst, other
+        return not inst == other
+    klass.__ne__ = __ne__
+
+    for k, v in vars(klass).iteritems():
+        if type(v) is constructor:
+            v.set_klass(klass)
+            v.set_name(k)
+    return klass
---
+++ test_sumtypes.py
@@ -0,0 +1,51 @@
+"""Tests for sumtypes."""
+
+from pytest import fixture
+
+from sumtypes import sumtype, constructor
+
+@sumtype
+class MyType(object):
+    MyConstructor = constructor(int)
+    @constructor
+    def AnotherConstructor(x, y):
+        assert type(x) is str
+        assert type(y) is int
+        return (x, y)
+
+
+@fixture
+def values():
+    return (MyType.MyConstructor(3),
+            MyType.AnotherConstructor('something', 1))
+
+def test_repr(values):
+    v, v2 = values
+    assert repr(v) == '<MyType.MyConstructor 3>'
+    assert repr(v2) == "<MyType.AnotherConstructor ('something', 1)>"
+
+def test_type(values):
+    v, v2 = values
+    assert type(v) is MyType
+    assert type(v2) is MyType
+
+def test_value(values):
+    v, v2 = values
+    assert v.value == 3
+    assert v2.value == ('something', 1)
+
+def test_constructor(values):
+    v, v2 = values
+    assert v.constructor is MyType.MyConstructor
+    assert v2.constructor is MyType.AnotherConstructor
+
+def test_equality(values):
+    v, v2 = values
+    assert v == MyType.MyConstructor(3)
+    assert v != MyType.MyConstructor(2)
+    assert not v != MyType.MyConstructor(3)
+    assert not v == MyType.MyConstructor(2)
+    assert v2 == MyType.AnotherConstructor('something', 1)
+    assert v2 != MyType.AnotherConstructor('otherthing', 2)
+    assert not v2 != MyType.AnotherConstructor('something', 1)
+    assert not v == MyType.AnotherConstructor('othering', 2)
