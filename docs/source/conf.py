261e5b5df6088f957b44fd02077178e62f8e3dd5
Christopher Armstrong <radix@twistedmatrix.com>
2015-05-29 15:02:16 -0500

    put some extensions back into the conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,3 +3,6 @@ project = 'sumtypes'
 copyright = '2015, Christopher Armstrong'
 author = 'Christopher Armstrong'
 version = release = '0.1a3'
+
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
